529320eeeca93d0d96af56c67680369ac5dd12df
Amirkh0n <a.devinnovator@gmail.com>
2025-06-15 16:01:51 +0500

    Add webhook

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from database import Users, Movies
 
-TOKEN = ""
+TOKEN = "5901209945:AAGQqHQmo5xOk-zlWYZJrNQOH48N8FmGoKU"
+WEBHOOK_URL = ""
 
 # part database
 DB_NAME = "database/database.db"
@@ -9,37 +10,37 @@ users = Users()
 movies = Movies()
 
 CHANNEL_ID = {
-    "short_video": ,
-    "full_video": ,
+    "short_video": -1002639346604,
+    "full_video": -1002639346604,
     "ads": {
         1: {
             "type": "channel",
-            "id": ,
+            "id": -1002639346604,
             "link": ""
         },
         2: {
             "type": "channel",
-            "id": ,
+            "id": -1002639346604,
             "link": ""
         },
         3: {
             "type": "channel",
-            "id": ,
+            "id": -1002639346604,
             "link": ""
         },
         4: {
             "type": "channel",
-            "id": ,
+            "id": -1002639346604,
             "link": ""
         },
         5: {
             "type": "channel",
-            "id": ,
+            "id": -1002639346604,
             "link": ""
         },
         6: {
             "type": "channel",
-            "id": ,
+            "id": -1002639346604,
             "link": ""
         }
     }
--- main.py
+++ main.py
@@ -1,23 +1,41 @@
+from fastapi import FastAPI, Request
+from telegram.ext.webhook import WebhookServer
+
+
 from telegram.ext import (
     Application, 
     CommandHandler,
     MessageHandler,
     filters
 )
-from config import TOKEN
+from config import TOKEN, WEBHOOK_URL
 import commands as cmd
 from models.main import on_startup, on_shutdown
 import messages as msg
 
+app = Application.builder().token(TOKEN).post_init(on_startup).post_shutdown(on_shutdown).build()
+app.add_handler(CommandHandler("start", cmd.start_handle))
+app.add_handler(MessageHandler(filters.TEXT, msg.message_handle))
+app.add_handler(MessageHandler(filters.VIDEO, msg.video_handle))
 
 def main():
-    app = Application.builder().token(TOKEN).post_init(on_startup).post_shutdown(on_shutdown).build()
-    app.add_handler(CommandHandler("start", cmd.start_handle))
-    app.add_handler(MessageHandler(filters.TEXT, msg.message_handle))
-    app.add_handler(MessageHandler(filters.VIDEO, msg.video_handle))
-
     print("Bot is running...!")
     app.run_polling()
-				
-if __name__ == "__main__":
-    main()
+
+
+web = FastAPI()
+
+@web.on_event("startup")
+async def startup():
+    await app.bot.set_webhook(url=WEBHOOK_URL)
+    print("Webhook ulandi")
+
+@web.post("/webhook")
+async def telegram_webhook(request: Request):
+    data = await request.json()
+    update = Update.de_json(data, app.bot)
+    await app.process_update(update)
+    return {"ok": True}
+
+#if __name__ == "__main__":
+#    main()
